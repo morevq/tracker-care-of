0c37a4500d3ddaa1f97269c2ec8047646c1b4dd2
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2026-01-27 18:03:46 +0300

    create aurh controller

---
+++ apps/api/include/controllers/auth-controller.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <optional>
+#include <nlohmann/json.hpp>
+
+namespace tracker_api {
+
+	struct AnamnesisResponse {
+		int id;
+		std::string description;
+		std::optional<std::string> photo_url;
+		std::string created_at;
+	};
+
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(AnamnesisResponse, id, description, photo_url, created_at)
+
+};
