36467cbc21cbbcd989f7a958d8c32e1ea947408d
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2026-01-27 15:30:21 +0300

    create water DTO

---
+++ apps/api/dto/water-dto.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+namespace tracker_api {
+
+	struct WaterIntakeResponse {
+		int id;
+		std::string lastWater;
+		int frequency;
+		std::string frequencyMeasure;
+	};
+	
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(WaterIntakeResponse, id, lastWater, frequency, frequencyMeasure)
+
+};
