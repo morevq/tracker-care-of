a9f655c768a831c830051e451474494d1aa4088e
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2026-01-27 15:23:23 +0300

    create patient DTO

---
+++ apps/api/dto/patient-dto.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <optional>
+#include <nlohmann/json.hpp>
+
+namespace tracker_api {
+	struct CreatePatientRequest {
+		std::string userUuid;
+		std::string name;
+		std::optional<std::string> birth_date;
+	};
+
+	struct PatientResponse {
+		int id;
+		std::string name;
+		std::optional<std::string> birth_date;
+		std::string created_at;
+	};
+
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(CreatePatientRequest, userUuid, name, birth_date)
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PatientResponse, id, name, birth_date, created_at)
+};
