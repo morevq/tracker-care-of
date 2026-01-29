ba4914f339f437be8f374a6813fa242dfde4e473
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2026-01-29 22:14:31 +0300

    add user_uuid into PatientResponse

--- apps/api/dto/patient-dto.h
+++ apps/api/dto/patient-dto.h
@@ -12,13 +12,13 @@ namespace tracker_api {
 	};
 
 	struct PatientResponse {
-		int id;
+		int id_patient;
+		std::string user_uuid;
 		std::string name;
 		std::optional<std::string> birth_date;
-		std::string created_at;
 	};
 
 	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(CreatePatientRequest, userUuid, name, birth_date)
-	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PatientResponse, id, name, birth_date, created_at)
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PatientResponse, id_patient, user_uuid, name, birth_date)
 
 };
